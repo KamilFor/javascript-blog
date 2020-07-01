2cc7cdc8f51dc8e72acd9c5d9dae9a6e2ee29a33
KamilFor <67376189+KamilFor@users.noreply.github.com>
2020-07-01 22:53:25 +0200

    temat Generujemy linki tagów w artykułach

--- js/script.js
+++ js/script.js
@@ -1,37 +1,92 @@
-"use strict";
-document.getElementById("test-button").addEventListener("click", function() {
-  const links = document.querySelectorAll(".titles a");
-  console.log("links:", links);
+'use strict';
+document.getElementById('test-button').addEventListener('click', function() {
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
 });
 const titleClickHandler = function(event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log("Link was clicked!");
+  const articleSelector = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('Link was clicked!');
 
   /* DONE remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll(".titles a.active");
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
-    activeLink.classList.remove("active");
+    activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
-  console.log("clickedElement:", clickedElement);
-  clickedElement.classList.add("active");
-
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
   /* DONE remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll(".posts article.active");
-
+  const activeArticles = document.querySelectorAll('.posts article.active');
   for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove("active");
+    activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+  console.log('articleSelector:', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  console.log('targetArticle:', targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 };
 
-const links = document.querySelectorAll(".titles a");
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
+
+function generateTitleLinks() {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log('titleList:', titleList);
+  titleList.innerHTML = '';
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('id:', articleId);
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log('articleTitle:', articleTitle);
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('link:', linkHTML);
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log('html:', linkHTML);
+  }
+
+  titleList.innerHTML = html;
+}
+generateTitleLinks();
+
+const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
+
+function generateTags() {
+  /* find all articles */
+  /* START LOOP: for every article: */
+  /* find tags wrapper */
+  /* make html variable with empty string */
+  /* get tags from data-tags attribute */
+  /* split tags into array */
+  /* START LOOP: for each tag */
+  /* generate HTML of the link */
+  /* add generated code to html variable */
+  /* END LOOP: for each tag */
+  /* insert HTML of all the links into the tags wrapper */
+  /* END LOOP: for every article: */
+}
+
+generateTags();
